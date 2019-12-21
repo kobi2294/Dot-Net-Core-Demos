397d5b5071b266fee7c01137351efac177385d33
Kobi Hari <kobi2294@yahoo.com>
2019-12-21 23:47:14 +0200

    Added Service that creates chart data upon request

---
+++ SignalRServer/SignalRServer.Web/Services/ChartDataService.cs
@@ -0,0 +1,38 @@
+﻿using SignalRServer.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRServer.Web.Services
+{
+    public class ChartDataService : IChartDataService
+    {
+        public int ChartModels { get; set; } = 4;
+
+        private Random _rand = new Random();
+
+        private ChartModel _generateModel(int i)
+        {
+            return new ChartModel
+            {
+                Data = new List<int> { _rand.Next(1, 40) },
+                Label = "Data" + i
+            };
+        }
+
+        private IEnumerable<ChartModel> _generateModels()
+        {
+            for (int i = 0; i < ChartModels; i++)
+            {
+                yield return _generateModel(i);
+            }
+        }
+
+        public Task<List<ChartModel>> GenerateData()
+        {
+            var res = _generateModels().ToList();
+            return Task.FromResult(res);
+        }
+    }
+}
---
+++ SignalRServer/SignalRServer.Web/Services/IChartDataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SignalRServer.Web.Models;
+
+namespace SignalRServer.Web.Services
+{
+    public interface IChartDataService
+    {
+        int ChartModels { get; set; }
+
+        Task<List<ChartModel>> GenerateData();
+    }
+}
