c54778487fd660c39ff3f0d83919af4e7f5f5799
Kobi Hari <kobi2294@yahoo.com>
2019-12-21 23:28:40 +0200

    Added Model entities to server

---
+++ SignalRServer/SignalRServer.Web/Models/ChartModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRServer.Web.Models
+{
+    public class ChartModel
+    {
+        public List<int> Data { get; set; }
+
+        public string Label { get; set; }
+
+        public ChartModel()
+        {
+            Data = new List<int>();
+        }
+    }
+}
